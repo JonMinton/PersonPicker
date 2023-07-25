b4f0118c48234742257c46343b2a0b33af31d321
Jon Minton <jon.will.minton@gmail.com>
2023-07-25 11:52:47 +0100

    get something basic and functional working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { FaTrash } from 'react-icons/fa';
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [highlightedUser, setHighlightedUser] = useState(null);
+  const [newUser, setNewUser] = useState('');
+
+  const handleAddUser = () => {
+    if (newUser !== '') {
+      setUsers([...users, newUser]);
+      setNewUser('');
+    }
+  };
+
+  const handleDeleteUser = (index) => {
+    const updatedUsers = [...users];
+    updatedUsers.splice(index, 1);
+    setUsers(updatedUsers);
+  };
+
+  const handleRandomize = () => {
+    const randomIndex = Math.floor(Math.random() * users.length);
+    setHighlightedUser(users[randomIndex]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React TardyTuesdays Volunteer Picker</h1>
+      <div>
+        <input
+          type="text"
+          value={newUser}
+          onChange={(e) => setNewUser(e.target.value)}
+        />
+        <button onClick={handleAddUser}>Submit</button>
+      </div>
+      <br></br>
+      <div>
+        <button onClick={handleRandomize}>Randomize</button>
+      </div>
+      <hr></hr>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index} style={highlightedUser === user ? { fontWeight: 'bold' } : {}}>
+            <FaTrash onClick={() => handleDeleteUser(index)} />
+            &nbsp;&nbsp;&nbsp; {user}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
